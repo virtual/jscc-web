f90c0d1e9c7416c5cdb5b42209250506715340cb
Jeanine Schoessler <jeanine.mt@gmail.com>
2016-11-13 07:17:15 -0700

    subfeature updates to match jscc

--- include-subfeature.php
+++ include-subfeature.php
@@ -29,29 +29,36 @@
         <!-- re -->
         <div class="flex-item ">
             <div>
-            <h2><a href="#">Plug in to jWeb</a></h2>
-            <p>Portal for students and employees. Access online courses and mail.</p>
-            <p><a class="action" href="https://lum5adminprod.jscc.edu:8443">Plug in to jWeb</a></p>
-                </div>
-        </div>
-        <div class="flex-item ">
-            <div>
-                <h2>Class Schedules</h2>
-            <div class="background-img" style="background-image:url('//www.easternflorida.edu/foundation/images/feature/slide-5-golf-tourney.png');">
-                <p><a href="#">3rd Annual Scholarship <strong>Golf Tournament &amp; Oktoberfest</strong></a> <a class="readmore" href="#">Learn more</a></p>
+                <h2><a href="https://lum5adminprod.jscc.edu:8443/">Plug in to jWeb</a></h2>
+                <p>
+                    <a href="https://lum5adminprod.jscc.edu:8443/"><img class="pull-right" src="_resources/images/subfeatures/hot-jweb.gif" alt="jWeb" /> Portal for students and employees. Access online courses and mail.</a>
+                </p>
+                <p><a class="action" href="https://lum5adminprod.jscc.edu:8443/">Plug in to jWeb</a></p>
             </div>
-                </div>
         </div>
-        <div class="flex-item ">
-            <div><h2><a href="#">JSCC Catalog</a></h2>
-            <p><a class="action" href="https://lum5adminprod.jscc.edu:8443">Another image</a></p>
-                </div>
-        </div>
-        <div class="flex-item ">
+        <div class="flex-item " style="background-image: url('./_resources/images/subfeatures/hot-schedule.jpg')">
             <div>
-            <h2><a href="#">Presidential Search</a></h2>
-            <p>This is just an image with a lil more texty text </p>
-           </div>
+                <h2><a href="http://www.jscc.edu/academics/calendars-schedules/class-schedule.html">Class Schedules</a></h2>
+                <div style="background-image:url('');">
+                    <p><a href="http://www.jscc.edu/schedule/springschedule.php">2017 Spring&nbsp;Schedule<br> </a><a href="http://www.jscc.edu/schedule/summerschedule.php">2016 Summer Schedule</a>
+                        <br><a href="http://www.jscc.edu/schedule/fallschedule.php">2016 Fall&nbsp;Schedule</a></p>
+                    <p><a class="action" href="http://www.jscc.edu/academics/calendars-schedules/class-schedule.html">See all class schedules</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="flex-item " style="background-color:#c9c6b2;">
+            <div>
+                <h2><a href="http://www.jscc.edu/admissions/registration/orientation.html">Orientation &amp; Registration</a></h2>
+                <a href="http://www.jscc.edu/admissions/registration/orientation.html"><img src="./_resources/images/subfeatures/hot-ge.jpg" class="Great Expectations"></a>
+                <p class="bottomlink"><a href="http://www.jscc.edu/admissions/registration/orientation.html">Sign Up Now</a>
+
+            </div>
+        </div>
+        <div class="flex-item " style="background-image: url('./_resources/images/subfeatures/pres_search-bk.jpg')">
+            <div>
+                <h2><a href="http://www.jscc.edu/about-jackson-state/administration/presidential-search.html">Presidential Search</a></h2>
+                <p style="text-align:center"><a href="http://www.jscc.edu/about-jackson-state/administration/presidential-search.html"><img src="./_resources/images/subfeatures/pres_search.png" alt="Jackson State Presidential Search"></a></p>
+            </div>
         </div>
     </div>
 
