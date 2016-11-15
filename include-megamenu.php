3c14d228d2a991a203d9d5b9375df693d853edee
Jeanine Schoessler <jeanine.mt@gmail.com>
2016-11-15 14:46:07 -0700

    more styling for top megamenu

--- include-megamenu.php
+++ include-megamenu.php
@@ -24,24 +24,40 @@
                 <div class="yamm-content">
                     <div class="row">
                       <div class="col-sm-12 link-list">
-                        <div class="header col-sm-12">
+                        
                           <h2>Financial Aid</h2>
-                            <p><a href="http://tennesseepromise.gov/"><img class="img_left imgshadow" src="./_resources/images/megamenu/megamenu-tnpromise.png" alt="TN Promise Logo" width="110" height="87">Learn what steps you need to follow for the Tennessee Promise.</a></p>
-                        </div>
-                        <ul class="greybox">
+                          <div class="row"><div class="col-sm-4"><img class="imgshadow" src="./_resources/images/megamenu/megamenu-tnpromise.png" alt="TN Promise Logo" width="110" height="87">
+                              
+                              </div><div class="col-sm-8">
+                              
+                               <p><a href="http://tennesseepromise.gov/">Learn what steps you need to follow for the Tennessee Promise.</a></p>
+                              </div>
+                          </div>
+                          <div class="buffer-top"><div class="row">
+                            <div class="col-sm-6">
+                                <ul class="greybox">
 <li><a onclick="ga('send', 'event', 'megamenu', ' aid', 'tuition');" href="/about-jackson-state/student-services/business-services/fees-tuition/fees-and-tuition.html">Tuition and Fees</a></li>
 <li><a onclick="ga('send', 'event', 'megamenu', 'financial aid', 'applications');" href="/financial-aid/forms-and-applications/index.html">Applications</a></li>
- 
+
+                                </ul></div>
+                              <div class="col-sm-6"><ul class="greybox">
+
 <li><a onclick="ga('send', 'event', 'megamenu', 'financial aid', 'deadlines');" href="/financial-aid/financial-aid-deadlines.html">Deadlines</a></li>
 <li><a onclick="ga('send', 'event', 'megamenu', 'financial aid', 'helpful resources');" href="/financial-aid/helpful-resources.html">Helpful Resources</a></li>
-</ul>
+</ul></div> 
+                              </div>
+                          </div>  
+                        
+                        
                           <p  ><a class="action" onclick="ga('send', 'event', 'megamenu', 'financial aid', 'see all');" href="/financial-aid/">See all Financial Aid</a></p>
                           
                         <h3>Types of Financial Aid</h3>
-                          
-                          
-                           <img class="img_left imgshadow" src="./_resources/images/megamenu/megamenu-financial.jpg" alt="Pile of cash and mortar board" width="70" height="70">
-<ul class="ul_left">
+                          <div class="row">
+                          <div class="col-sm-4">
+                              <img class="imgshadow" src="./_resources/images/megamenu/megamenu-financial.jpg" alt="Pile of cash and mortar board" width="70" height="70">
+                          </div>
+                          <div class="col-sm-8">
+                              <ul class="ul_left list-unstyled">
 <li><a onclick="ga('send', 'event', 'megamenu', 'financial aid', 'fed/state');" href="/financial-aid/apply-for-federal-aid.html">Federal/State - Pell Grants</a></li>
 <li><a onclick="ga('send', 'event', 'megamenu', 'financial aid', 'scholarships');" href="/financial-aid/types-of-aid/scholarships.html">Scholarships</a></li>
 <li><a onclick="ga('send', 'event', 'megamenu', 'financial aid', 'lottery');" href="/financial-aid/types-of-aid/lottery.html">Lottery</a></li>
@@ -49,6 +65,11 @@
 <li><a onclick="ga('send', 'event', 'megamenu', 'financial aid', 'loans');" href="/financial-aid/types-of-aid/private-loans.html">Loans</a></li>
 <li><a onclick="ga('send', 'event', 'megamenu', 'financial aid', 'all types');" href="/financial-aid/types-of-aid/">See all Types of Aid</a></li>
 </ul> 
+                          </div>
+                          </div>
+                          
+                           
+
                           
                       </div>
                       
