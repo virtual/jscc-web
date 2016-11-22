bae0b34d7cd144d3166cf5f5a946c6ed1011673e
Jeanine Schoessler <jeanine.mt@gmail.com>
2016-11-22 06:26:40 -0700

    format megamenu

--- include-megamenu.php
+++ include-megamenu.php
@@ -23,64 +23,70 @@
                 <!-- Content container to add padding -->
                 <div class="yamm-content">
                     <div class="row">
-                      <div class="col-sm-12 link-list">
-                        
-                          <h2>Financial Aid</h2>
-                          <div class="row hidemobile"><div class="col-sm-6"><img class="imgshadow" src="./_resources/images/megamenu/tnpromise-logo.png" alt="TN Promise Logo" >
-                              
-                              </div><div class="col-sm-6">
-                              
-                               <p><a href="http://tennesseepromise.gov/">Learn what steps you need to follow for the Tennessee Promise.</a></p>
-                              </div>
-                          </div>
-                          <div class="buffer-top"><div class="row">
-                            <div class="col-sm-6">
-                                <ul class="greybox">
-<li><a onclick="ga('send', 'event', 'megamenu', ' aid', 'tuition');" href="/about-jackson-state/student-services/business-services/fees-tuition/fees-and-tuition.html">Tuition and Fees</a></li>
-<li><a onclick="ga('send', 'event', 'megamenu', 'financial aid', 'applications');" href="/financial-aid/forms-and-applications/index.html">Applications</a></li>
+                        <div class="col-sm-12 link-list">
 
-                                </ul></div>
-                              <div class="col-sm-6"><ul class="greybox">
+                            <h2>Financial Aid</h2>
+                            <div class="row hidemobile">
+                                <div class="col-sm-6"><img class="imgshadow" src="./_resources/images/megamenu/tnpromise-logo.png" alt="TN Promise Logo">
 
-<li><a onclick="ga('send', 'event', 'megamenu', 'financial aid', 'deadlines');" href="/financial-aid/financial-aid-deadlines.html">Deadlines</a></li>
-<li><a onclick="ga('send', 'event', 'megamenu', 'financial aid', 'helpful resources');" href="/financial-aid/helpful-resources.html">Helpful Resources</a></li>
-</ul></div> 
-                              </div>
-                          </div>  
-                        
-                        
-                          <p  ><a class="action" onclick="ga('send', 'event', 'megamenu', 'financial aid', 'see all');" href="/financial-aid/">See all Financial Aid</a></p>
-                          
-                        <h3>Types of Financial Aid</h3>
-                          <div class="row">
-                          <div class="col-sm-4 col-xs-12 hidemobile">
-                              <img class="imgshadow" src="./_resources/images/megamenu/megamenu-financial.jpg" alt="Pile of cash and mortar board" >
-                          </div>
-                          <div class="col-sm-8 col-xs-12">
-                              <ul class="ul_left list-unstyled">
-<li><a onclick="ga('send', 'event', 'megamenu', 'financial aid', 'fed/state');" href="/financial-aid/apply-for-federal-aid.html">Federal/State - Pell Grants</a></li>
-<li><a onclick="ga('send', 'event', 'megamenu', 'financial aid', 'scholarships');" href="/financial-aid/types-of-aid/scholarships.html">Scholarships</a></li>
-<li><a onclick="ga('send', 'event', 'megamenu', 'financial aid', 'lottery');" href="/financial-aid/types-of-aid/lottery.html">Lottery</a></li>
-<li><a onclick="ga('send', 'event', 'megamenu', 'financial aid', 'work study');" href="/financial-aid/types-of-aid/employment.html">Work Study</a></li>
-<li><a onclick="ga('send', 'event', 'megamenu', 'financial aid', 'loans');" href="/financial-aid/types-of-aid/private-loans.html">Loans</a></li>
-<li><a onclick="ga('send', 'event', 'megamenu', 'financial aid', 'all types');" href="/financial-aid/types-of-aid/">See all Types of Aid</a></li>
-</ul> 
-                          </div>
-                          </div>
-                          
-                           
+                                </div>
+                                <div class="col-sm-6">
 
-                          
-                      </div>
-                      
+                                    <p><a href="http://tennesseepromise.gov/">Learn what steps you need to follow for the Tennessee Promise.</a></p>
+                                </div>
+                            </div>
+                            <div class="buffer-top">
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <ul class="greybox">
+                                            <li><a onclick="ga('send', 'event', 'megamenu', ' aid', 'tuition');" href="/about-jackson-state/student-services/business-services/fees-tuition/fees-and-tuition.html">Tuition and Fees</a></li>
+                                            <li><a onclick="ga('send', 'event', 'megamenu', 'financial aid', 'applications');" href="/financial-aid/forms-and-applications/index.html">Applications</a></li>
+
+                                        </ul>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <ul class="greybox">
+
+                                            <li><a onclick="ga('send', 'event', 'megamenu', 'financial aid', 'deadlines');" href="/financial-aid/financial-aid-deadlines.html">Deadlines</a></li>
+                                            <li><a onclick="ga('send', 'event', 'megamenu', 'financial aid', 'helpful resources');" href="/financial-aid/helpful-resources.html">Helpful Resources</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <p><a class="action" onclick="ga('send', 'event', 'megamenu', 'financial aid', 'see all');" href="/financial-aid/">See all Financial Aid</a></p>
+
+                            <h3>Types of Financial Aid</h3>
+                            <div class="row">
+                                <div class="col-sm-4 col-xs-12 hidemobile">
+                                    <img class="imgshadow" src="./_resources/images/megamenu/megamenu-financial.jpg" alt="Pile of cash and mortar board">
+                                </div>
+                                <div class="col-sm-8 col-xs-12">
+                                    <ul class="ul_left list-unstyled">
+                                        <li><a onclick="ga('send', 'event', 'megamenu', 'financial aid', 'fed/state');" href="/financial-aid/apply-for-federal-aid.html">Federal/State - Pell Grants</a></li>
+                                        <li><a onclick="ga('send', 'event', 'megamenu', 'financial aid', 'scholarships');" href="/financial-aid/types-of-aid/scholarships.html">Scholarships</a></li>
+                                        <li><a onclick="ga('send', 'event', 'megamenu', 'financial aid', 'lottery');" href="/financial-aid/types-of-aid/lottery.html">Lottery</a></li>
+                                        <li><a onclick="ga('send', 'event', 'megamenu', 'financial aid', 'work study');" href="/financial-aid/types-of-aid/employment.html">Work Study</a></li>
+                                        <li><a onclick="ga('send', 'event', 'megamenu', 'financial aid', 'loans');" href="/financial-aid/types-of-aid/private-loans.html">Loans</a></li>
+                                        <li><a onclick="ga('send', 'event', 'megamenu', 'financial aid', 'all types');" href="/financial-aid/types-of-aid/">See all Types of Aid</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+
+
+
+
+                        </div>
+
                     </div>
-                  </div>
+                </div>
             </li>
         </ul>
     </li>
     <li class="dropdown yamm-fw"><a href="#" data-toggle="dropdown" class="dropdown-toggle"><span class="fa fa-caret-right" aria-hidden="false"><span class="sr-only">Expand</span></span>Academics</a>
         <ul class="dropdown-menu">
-            <li class="container"> 
+            <li class="container">
                 <div class="yamm-content">
                     <div class="row">
 
@@ -142,71 +148,8 @@
                 </div>
             </li>
         </ul>
-    </li>    <li class="dropdown yamm-fw"><a href="#" data-toggle="dropdown" class="dropdown-toggle"><span class="fa fa-caret-right" aria-hidden="false"><span class="sr-only">Expand</span></span>Athletics</a>
-        <ul class="dropdown-menu">
-            <li class="container">
-                <!-- Content container to add padding -->
-                <div class="yamm-content">
-                    <div class="row">
-
-                        <ul class="col-sm-12 list-unstyled">
-                            <li class="dropdown-submenu">
-                                <a tabindex="0" data-toggle="dropdown"><span class="fa fa-caret-right" aria-hidden="false"><span class="sr-only">Expand</span></span> Office of the President</a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Board of Regents</a></li>
-                                    <li><a href="#">Board Postings</a></li>
-                                </ul>
-                            </li>
-
-                            <li class="dropdown-submenu">
-                                <a tabindex="0" data-toggle="dropdown"><span class="fa fa-caret-right" aria-hidden="false"><span class="sr-only">Expand</span></span> Campus Locations</a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Campus Maps</a></li>
-                                </ul>
-                            </li>
-
-                            <li><a href="#">News &amp; Events</a></li>
-                            <li><a href="#">Faculty &amp; Staff Directory</a></li>
-                            <li><a href="#">Human Resources</a></li>
-                        </ul>
-
-                    </div>
-                </div>
-            </li>
-        </ul>
-    </li>    <li class="dropdown yamm-fw"><a href="#" data-toggle="dropdown" class="dropdown-toggle"><span class="fa fa-caret-right" aria-hidden="false"><span class="sr-only">Expand</span></span>About JSCC</a>
-        <ul class="dropdown-menu">
-            <li class="container">
-                <!-- Content container to add padding -->
-                <div class="yamm-content">
-                    <div class="row">
-
-                        <ul class="col-sm-12 list-unstyled">
-                            <li class="dropdown-submenu">
-                                <a tabindex="0" data-toggle="dropdown"><span class="fa fa-caret-right" aria-hidden="false"><span class="sr-only">Expand</span></span> Office of the President</a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Board of Regents</a></li>
-                                    <li><a href="#">Board Postings</a></li>
-                                </ul>
-                            </li>
-
-                            <li class="dropdown-submenu">
-                                <a tabindex="0" data-toggle="dropdown"><span class="fa fa-caret-right" aria-hidden="false"><span class="sr-only">Expand</span></span> Campus Locations</a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Campus Maps</a></li>
-                                </ul>
-                            </li>
-
-                            <li><a href="#">News &amp; Events</a></li>
-                            <li><a href="#">Faculty &amp; Staff Directory</a></li>
-                            <li><a href="#">Human Resources</a></li>
-                        </ul>
-
-                    </div>
-                </div>
-            </li>
-        </ul>
-    </li>    <li class="dropdown yamm-fw"><a href="#" data-toggle="dropdown" class="dropdown-toggle"><span class="fa fa-caret-right" aria-hidden="false"><span class="sr-only">Expand</span></span>Community Outreach</a>
+    </li>
+    <li class="dropdown yamm-fw"><a href="#" data-toggle="dropdown" class="dropdown-toggle"><span class="fa fa-caret-right" aria-hidden="false"><span class="sr-only">Expand</span></span>Athletics</a>
         <ul class="dropdown-menu">
             <li class="container">
                 <!-- Content container to add padding -->
@@ -239,6 +182,72 @@
             </li>
         </ul>
     </li>
-  
+    <li class="dropdown yamm-fw"><a href="#" data-toggle="dropdown" class="dropdown-toggle"><span class="fa fa-caret-right" aria-hidden="false"><span class="sr-only">Expand</span></span>About JSCC</a>
+        <ul class="dropdown-menu">
+            <li class="container">
+                <!-- Content container to add padding -->
+                <div class="yamm-content">
+                    <div class="row">
+
+                        <ul class="col-sm-12 list-unstyled">
+                            <li class="dropdown-submenu">
+                                <a tabindex="0" data-toggle="dropdown"><span class="fa fa-caret-right" aria-hidden="false"><span class="sr-only">Expand</span></span> Office of the President</a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Board of Regents</a></li>
+                                    <li><a href="#">Board Postings</a></li>
+                                </ul>
+                            </li>
+
+                            <li class="dropdown-submenu">
+                                <a tabindex="0" data-toggle="dropdown"><span class="fa fa-caret-right" aria-hidden="false"><span class="sr-only">Expand</span></span> Campus Locations</a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Campus Maps</a></li>
+                                </ul>
+                            </li>
+
+                            <li><a href="#">News &amp; Events</a></li>
+                            <li><a href="#">Faculty &amp; Staff Directory</a></li>
+                            <li><a href="#">Human Resources</a></li>
+                        </ul>
+
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </li>
+    <li class="dropdown yamm-fw"><a href="#" data-toggle="dropdown" class="dropdown-toggle"><span class="fa fa-caret-right" aria-hidden="false"><span class="sr-only">Expand</span></span>Community Outreach</a>
+        <ul class="dropdown-menu">
+            <li class="container">
+                <!-- Content container to add padding -->
+                <div class="yamm-content">
+                    <div class="row">
+
+                        <ul class="col-sm-12 list-unstyled">
+                            <li class="dropdown-submenu">
+                                <a tabindex="0" data-toggle="dropdown"><span class="fa fa-caret-right" aria-hidden="false"><span class="sr-only">Expand</span></span> Office of the President</a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Board of Regents</a></li>
+                                    <li><a href="#">Board Postings</a></li>
+                                </ul>
+                            </li>
+
+                            <li class="dropdown-submenu">
+                                <a tabindex="0" data-toggle="dropdown"><span class="fa fa-caret-right" aria-hidden="false"><span class="sr-only">Expand</span></span> Campus Locations</a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Campus Maps</a></li>
+                                </ul>
+                            </li>
+
+                            <li><a href="#">News &amp; Events</a></li>
+                            <li><a href="#">Faculty &amp; Staff Directory</a></li>
+                            <li><a href="#">Human Resources</a></li>
+                        </ul>
+
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </li>
+
 
 </ul>
